a5d2520d653d98bbef7c492f3eb2f745ce36ca70
codrixer <maweujastus@gmail.com>
2025-04-01 06:44:14 -0400

    Update Header.tsx
    changed to my text liking

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-import { BookMarkedIcon, BookOpen } from "lucide-react";
+import { Terminal, SquareDashedBottomCode } from "lucide-react";
 import Link from "next/link";
 import { SearchInput } from "./SearchInput";
 import { Button } from "./ui/button";
@@ -18,9 +18,9 @@ export default function Header() {
               prefetch={false}
               className="flex items-center space-x-2 hover:opacity-90 transition-opacity"
             >
-              <BookOpen className="h-6 w-6 text-primary" />
+              <SquareDashedBottomCode className="h-8 w-8"/>
               <span className="text-xl font-bold bg-gradient-to-r from-primary/90 to-primary bg-clip-text text-transparent">
-                Courselly
+                DevTube
               </span>
             </Link>
 
@@ -34,8 +34,8 @@ export default function Header() {
                 href="/my-courses"
                 className="flex space-x-2 items-center text-sm font-medium text-muted-foreground hover:text-foreground transition-colors md:border md:border-border md:rounded-md md:px-4 md:py-2"
               >
-                <BookMarkedIcon className="h-4 w-4" />
-                <span className="hidden md:block">My Courses</span>
+                <Terminal className="h-4 w-4" />
+                <span className="hidden md:block">Tutorials</span>
               </Link>
             </nav>
 
