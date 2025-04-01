86a4368bc9f211f06cbe05f22e8b1ead02c22936
codrixer <maweujastus@gmail.com>
2025-04-01 06:51:15 -0400

    Update page.tsx

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -17,7 +17,7 @@ export default async function Home() {
         <div className="flex items-center gap-4 py-8">
           <div className="h-px flex-1 bg-gradient-to-r from-border/0 via-border to-border/0" />
           <span className="text-sm font-medium text-muted-foreground">
-            Featured Courses
+            Featured Tutorials
           </span>
           <div className="h-px flex-1 bg-gradient-to-r from-border/0 via-border to-border/0" />
         </div>
